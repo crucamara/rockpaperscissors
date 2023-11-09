83d8650a0f188c4bb53520694477ce3989d48f9c
Your Name <yourname@example.comgit config --global user.name Cru Camaragit config --global user.email cru.camara@gmail.com>
2023-11-09 18:49:22 +0800

    Fix playRound to stop returning tie
    Defined getPlayerChoice and getComputerChoice
    Logged playRound function to confirm if both functions worked properly

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const options = ["rock", "paper", "scissors"]
 message = "rock, paper, or scissors? type your choice below."
 
+
 function getComputerChoice() {
     let computerChoice = options[Math.floor(Math.random() * options.length)]
     console.log(computerChoice)
@@ -20,21 +21,22 @@ function getPlayerChoice() {
     return playerChoice
 }
 
+function playRound(playerChoice, computerChoice) {
 
-function round1(playerChoice, computerChoice) {
-
-    if(playerChoice == computerChoice) {
-        return "It's a tie!"
-    } 
-    else if(playerChoice == "rock" && computerChoice == "scissors" || 
+    if(playerChoice == "rock" && computerChoice == "scissors" || 
     playerChoice == "paper" && computerChoice == "rock" ||
     playerChoice == "scissors" && computerChoice == "rock") {
         return "You win!"
+    }
+    else if(playerChoice === computerChoice) {
+        return "It's a tie!"
+
     }
     else {
         return "You lose!"
     }
 }
 
-
-round1()
+let playerChoice = getPlayerChoice()
+let computerChoice = getComputerChoice()
+console.log(playRound(playerChoice, computerChoice))
